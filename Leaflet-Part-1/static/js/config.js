7d53640b17363f26e137a242dec40ce4bdb0a948
Erin Clark <ehclark2020@gmail.com>
2023-04-11 17:04:51 -0500

    completed code

---
+++ Leaflet-Part-1/static/js/config.js
@@ -0,0 +1,2 @@
+//query endpoint variable
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,81 @@
+//create base level map
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 4
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+//get request to the query URL and add to map
+d3.json(url).then(function (data) {
+    function mapStyle(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.geometry.coordinates[2]),
+            color: "grey",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+    //color change by depth
+    function mapColor(depth) {
+        switch (true) {
+            case depth > 90:
+                return "darkpurple";
+            case depth > 70:
+                return "darkblue";
+            case depth > 50:
+                return "darkgreen";
+            case depth > 30:
+                return "green";
+            case depth > 10:
+                return "lightgreen";
+            default:
+                return "yellow";
+        }
+    }
+    //change size based on magnitude
+    function mapRadius(mag) {
+        if (mag === 0) {
+            return 1;
+        }
+            return mag * 4;
+    }
+     //add earthquakes to the map
+    L.geoJson(data, {
+
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+
+        style: mapStyle,
+
+        //pop-up data when circles are clicked
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
+
+        }
+    }).addTo(myMap);
+//add legend with colors
+var legend = L.control({position: "topright"});
+legend.onAdd = function() {
+  var div = L.DomUtil.create("div", "info legend"),
+  depth = [-10, 10, 30, 50, 70, 90];
+  var labels = [];
+
+
+  for (var i = 0; i < depth.length; i++) {
+    div.innerHTML += '<i style="background-color:' + mapColor(depth[i] + 1) + '"> <span>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '' : '+' + '</span></ul>');
+}
+
+div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+};
+
+legend.addTo(myMap)
+});
